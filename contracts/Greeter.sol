d17f26c14c494fc30464beef423dffe8001aa0c3
Ruth Chisom <ndukwe100805043@gmail.com>
2023-05-10 11:43:52 +0100

    write smart contract and test

---
+++ contracts/Greeter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Greeter {
+    string greeting;
+
+    constructor(string memory _greeting) {
+        greeting = _greeting;
+    }
+
+    function greet() public  view returns (string memory){
+        return greeting;
+    }
+}
---
+++ test/Greeter.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+
+describe("Greeter", function () {
+    it("Should return the greeting", async function () {
+      const Greeter = await ethers.getContractFactory('Greeter');
+      const greeter = await Greeter.deploy('Hello, world!');
+      await greeter.deployed();
+
+      expect(await greeter.greet()).to.equal('Hello, world!');
+    });
+
+  });
